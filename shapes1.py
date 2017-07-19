7b84e25c02a9e343aef1586bc7928b808351178c
masfi-khan <khanmasfi2000@gmail.com>
2017-07-19 14:20:23 -0400

    Update shapes1.py

--- shapes1.py
+++ shapes1.py
@@ -5,15 +5,33 @@ import math
 tutu = Turtle()
 
 # Set Up your screen and starting position.
-setup(500,300)
-x_pos = -250
-y_pos = -150
+setup(50,50)
+x_pos = 50
+y_pos = 50
 
 ### Write your code below:
-tutu.forward(150)
-tutu.right(90)
-tutu.forward(150)
-tutu.left(90)
-tutu.backward(150)
-tutu.right(90)
-tutu.backward(150)
+###tutu.forward(150)
+# tutu.right(90)
+# tutu.forward(150)
+# tutu.left(90)
+# tutu.backward(150)
+# tutu.right(90)
+# tutu.backward(150)
+
+number = int(input ("Please write how many sides the polygon has"))
+color = input ("Choose a filling color")
+tutu.color("color")
+tutu.begin_fill()
+def shape():
+    while number < number + 1:
+        tutu.forward(100)
+        tutu.left(360/number)
+tutu.end_fill()
+
+shape()
+
+
+
+# Close window on click.
+exitonclick()
+
