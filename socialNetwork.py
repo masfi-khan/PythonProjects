01cd6ebd46a84be6b55c60460279ef134ff4c0ee
masfi-khan <khanmasfi2000@gmail.com>
2017-08-09 15:50:15 -0400

    Create socialNetwork.py

---
+++ socialNetwork.py
@@ -0,0 +1,41 @@
+import turtle
+import math
+
+class User:
+    def _init_(self, name):
+        self.name = username
+        self.connections = []
+
+    def id(self, id):
+        self.account = []
+        n = len(self.account)
+
+        for i in range(n):
+            i += 1
+            self.id = self.account[i]
+            self.account.append(self.id)
+
+    def connections(self, connections):
+        self.connections.append(friend)
+
+    def get_username(self):
+        return self.name
+
+    def get_id(self):
+        return self.id
+
+    def get_connections(self):
+        return self.connections
+
+
+class Network:
+    # Define the fields and methods for your object here.
+
+
+def main():
+    # Define the program flow for your user interface here.
+
+
+# Runs your program.
+if __name__ == '__main__':
+    main():
